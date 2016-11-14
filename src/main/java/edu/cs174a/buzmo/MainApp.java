3d34364fa7e0544f2e47532d2f1fb5641e98fc37
Sal Olivares <git@salolivares.com>
2016-11-13 22:28:57 -0800

    created folder structure and login view

---
+++ src/main/java/edu/cs174a/buzmo/MainApp.java
@@ -0,0 +1,73 @@
+package edu.cs174a.buzmo;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Buzmo");
+
+        initRootLayout();
+
+        showLoginScreen();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            primaryStage.setScene(new Scene(rootLayout));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showLoginScreen() {
+        try {
+            // Load Login Layout.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("/view/LoginLayout.fxml"));
+            AnchorPane loginLayout = (AnchorPane) loader.load();
+
+            // Set login layout into the center of root layout.
+            rootLayout.setCenter(loginLayout);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
