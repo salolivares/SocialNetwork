d54712eeda938555ff64f225314331196ce262e2
Sal Olivares <git@salolivares.com>
2016-11-13 21:39:59 -0800

    Initial Commit. Source Files generated with Maven Archtype

---
+++ src/main/java/edu/cs174a/buzmo/App.java
@@ -0,0 +1,13 @@
+package edu.cs174a.buzmo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
