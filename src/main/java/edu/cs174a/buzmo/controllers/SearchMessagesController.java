326b2138e72f50966c6cf328b027fbf8eaa75ebd
Jordan Nguyen <jordannguyen95@gmail.com>
2016-11-30 21:53:58 -0800

    can add topic words to search messages

--- src/main/java/edu/cs174a/buzmo/controllers/SearchMessagesController.java
+++ src/main/java/edu/cs174a/buzmo/controllers/SearchMessagesController.java
@@ -1,9 +1,13 @@
 package edu.cs174a.buzmo.controllers;
 
 import edu.cs174a.buzmo.MainApp;
+import edu.cs174a.buzmo.tasks.CreateTopicTask;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 /**
  * Created by jordannguyen on 11/30/16.
@@ -13,14 +17,24 @@ public class SearchMessagesController {
     @FXML private Button backButton;
     @FXML private Button addTopicButton;
     @FXML private Button searchButton;
-
+    @FXML private ListView<String> topicsList;
+    @FXML private TextField topicsTextField;
 
     @FXML private void initialize() {
         backButton.setOnAction(this::handleBackAction);
+        addTopicButton.setOnAction(this::handleAddTopicAction);
     }
 
     private void handleBackAction(ActionEvent actionEvent) {
         mainApp.getGUIManager().showMyCircleMenuLayout();
+    }
+
+    private void handleAddTopicAction(ActionEvent actionEvent) {
+        if (!topicsTextField.equals("") && !topicsList.getItems().contains(topicsTextField.getText())) {
+            ObservableList<String> newList = topicsList.getItems();
+            newList.add(topicsTextField.getText());
+            topicsList.setItems(newList);
+        }
     }
 
     public void setMainApp(MainApp mainApp) {
