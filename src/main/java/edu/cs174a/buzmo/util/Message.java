11fe1dc255a85afb97c514def12405a08a5ffa6d
Jordan Nguyen <jordannguyen95@gmail.com>
2016-12-01 00:24:41 -0800

    basic message class

---
+++ src/main/java/edu/cs174a/buzmo/util/Message.java
@@ -0,0 +1,18 @@
+package edu.cs174a.buzmo.util;
+
+/**
+ * Created by jordannguyen on 12/1/16.
+ */
+public class Message {
+    private String body;
+    private String sender;
+
+    public Message(String sender, String body) {
+        this.body = body;
+        this.sender = sender;
+    }
+
+    public String getBody() {return this.body;}
+    public String getSender() {return this.sender;}
+    
+}
