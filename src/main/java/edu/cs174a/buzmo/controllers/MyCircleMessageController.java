65e15b7956566ec38d892cd682d5a220fc7a41e0
Jordan Nguyen <jordannguyen95@gmail.com>
2016-12-01 18:34:45 -0800

    working on querying mycircle messages

--- src/main/java/edu/cs174a/buzmo/controllers/MyCircleMessageController.java
+++ src/main/java/edu/cs174a/buzmo/controllers/MyCircleMessageController.java
@@ -3,15 +3,14 @@ package edu.cs174a.buzmo.controllers;
 import edu.cs174a.buzmo.MainApp;
 import edu.cs174a.buzmo.tasks.CreateTopicTask;
 import edu.cs174a.buzmo.tasks.FetchFriendsTask;
+import edu.cs174a.buzmo.tasks.FetchMyCircleMessagesTask;
+import edu.cs174a.buzmo.util.Message;
 import edu.cs174a.buzmo.util.ProgressSpinner;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 /**
  * Created by jordannguyen on 11/30/16.
@@ -23,7 +22,7 @@ public class MyCircleMessageController {
     @FXML private Button backButton;
     @FXML private Button refreshButton;
     @FXML private ListView<String> topicListView;
-    @FXML private ListView<?> messageList;
+    @FXML private ListView<Message> messageList;
     @FXML private ChoiceBox<String> publicChoiceBox;
     @FXML private Button addTopicButton;
     @FXML private TextField topicTextField;
@@ -38,6 +37,18 @@ public class MyCircleMessageController {
         backButton.setOnAction(this::handleBackAction);
         refreshButton.setOnAction(this::handleRefreshAction);
         addTopicButton.setOnAction(this::handleAddTopicAction);
+        messageList.setCellFactory(param -> new ListCell<Message>() {
+            @Override
+            protected void updateItem(Message item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty || item == null || item.getBody() == null) {
+                    setText(null);
+                } else {
+                    setText("[" + item.getTimestamp().substring(5, 19) + "] " + item.getSender() + ": " + item.getBody());
+                }
+            }
+        });
     }
 
     private void handleBackAction(ActionEvent actionEvent) {
@@ -46,7 +57,7 @@ public class MyCircleMessageController {
 
     private void handleRefreshAction(ActionEvent actionEvent) {
         System.out.println("Refreshing...");
-        //populateMessages();
+        populateMessages();
         System.out.println("Populated!");
     }
 
@@ -61,16 +72,15 @@ public class MyCircleMessageController {
         }
     }
 
-/*
     private void populateMessages() {
         ProgressSpinner ps = new ProgressSpinner(mainApp.getRootLayout());
         ps.startSpinner();
 
-        final FetchMessagesTask fetchMessages = new FetchMessages(mainApp.getGUIManager().getEmail());
+        final FetchMyCircleMessagesTask fetchMessages = new FetchMyCircleMessagesTask(mainApp.getGUIManager().getEmail());
 
         fetchMessages.setOnSucceeded(t -> {
             Platform.runLater(ps::stopSpinner);
-            friendsList.setItems(fetchMessages.getValue());
+            messageList.setItems(fetchMessages.getValue());
         });
 
         fetchMessages.setOnFailed(t -> {
@@ -80,7 +90,6 @@ public class MyCircleMessageController {
 
         mainApp.getDatabaseExecutor().submit(fetchMessages);
     }
-*/
 
     public void refreshMessages(){
         refreshButton.fire();
