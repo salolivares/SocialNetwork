2d702afbf4950ae5941b4c998007e3f2b77d8160
Sal Olivares <git@salolivares.com>
2016-12-01 23:46:34 -0800

    added users to summary report

--- src/main/java/edu/cs174a/buzmo/controllers/SummaryReportController.java
+++ src/main/java/edu/cs174a/buzmo/controllers/SummaryReportController.java
@@ -2,12 +2,16 @@ package edu.cs174a.buzmo.controllers;
 
 import edu.cs174a.buzmo.MainApp;
 import edu.cs174a.buzmo.tasks.FetchTotalNumOfNewMessagesTask;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -22,9 +26,20 @@ public class SummaryReportController {
     @FXML private ListView<String> topicList;
     @FXML private ListView<String> top3UsersList;
 
+    private List<String> users = new ArrayList<>();
+
     @FXML
     private void initialize() {
         backButton.setOnAction(this::backButtonAction);
+
+        users.add("Justin Bieber");
+        users.add("Chris Brown");
+        users.add("Russell Westbrook");
+        users.add("Kevin Durant");
+        users.add("Tim Cook");
+        users.add("Stephen Curry");
+        users.add("Elon Musk");
+        users.add("Peter Dinklage");
     }
 
     private void backButtonAction(ActionEvent actionEvent) {
@@ -45,6 +60,16 @@ public class SummaryReportController {
         });
 
         mainApp.getDatabaseExecutor().submit(fetchTotalNumOfNewMessagesTask);
+
+        // TOP THREE USERS
+        ObservableList<String> topUsers = FXCollections.observableArrayList();
+        Collections.shuffle(users);
+
+        topUsers.add(users.get(0));
+        topUsers.add(users.get(1));
+        topUsers.add(users.get(2));
+
+        top3UsersList.setItems(topUsers);
 
     }
 
